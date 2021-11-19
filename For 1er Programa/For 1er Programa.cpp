c9ae1aade46e5f9ad18936e233be410bde7c72f9
arash <arash@ARASHITORA>
2021-11-19 01:34:24 -0400

    Add project files.

---
+++ For 1er Programa/For 1er Programa.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() // Sumara de 2 en 2 hasta el 100
+{
+    for (int i = 50;i <= 100;i += 2)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
